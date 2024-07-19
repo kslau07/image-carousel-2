3e5b91601f1c6251f9b3cb7219680ee337569ef5
sam <abluecar@gmail.com>
2024-07-19 01:36:58 -0400

    feat: Nav dots and buttons change to target slide

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './style.css';
 
 const track = document.querySelector('.carousel__track');
 const slides = [...track.children];
-
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 const setSlideWidth = (slide, index) => {
@@ -12,22 +11,51 @@ const setSlideWidth = (slide, index) => {
 };
 slides.forEach(setSlideWidth);
 
+const nav = document.querySelector('.carousel__nav');
+const dots = [...nav.children];
+
+const updateDotColors = function (oldSlideIndex, newSlideIndex) {
+  dots[oldSlideIndex].style.background = '#cccccc';
+  dots[newSlideIndex].style.background = '#222222';
+};
+
+const setCurrentSlide = (newSlideIndex) => {
+  track.style.left = `-${slideWidth * newSlideIndex}px`;
+  const oldSlide = track.querySelector('[data-current-slide]');
+  const oldSlideIndex = slides.indexOf(oldSlide);
+  updateDotColors(oldSlideIndex, newSlideIndex);
+  slides[newSlideIndex].dataset.currentSlide = 'true';
+  delete oldSlide.dataset.currentSlide;
+};
+
 const buttonLeft = document.querySelector('.carousel__button--left');
 const buttonRight = document.querySelector('.carousel__button--right');
 
-// Move top image left by 100px when we press left
+const viewNextSlide = function () {
+  const currentSlide = track.querySelector('[data-current-slide]');
+  const currentIndex = slides.indexOf(currentSlide);
+  const prevIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+  setCurrentSlide(prevIndex);
+};
 
-buttonRight.addEventListener('click', () => {
+buttonLeft.addEventListener('click', viewNextSlide);
+
+const viewPrevSlide = function () {
   const currentSlide = track.querySelector('[data-current-slide]');
   const currentIndex = slides.indexOf(currentSlide);
   const nextIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
-  const nextSlide = slides[nextIndex];
+  setCurrentSlide(nextIndex);
+};
 
-  track.style.left = `-${slideWidth * nextIndex}px`;
-  nextSlide.dataset.currentSlide = true;
-  delete currentSlide.dataset.currentSlide;
-});
+buttonRight.addEventListener('click', viewPrevSlide);
 
-// Line up images side by side on the x-axis
-// When we press right, shift all images left by x amount
-// When we press left, shift all images right by x amount
+const setDot = function (dot) {
+  dot.addEventListener('click', () => {
+    const idx = dots.indexOf(dot);
+    setCurrentSlide(idx);
+  });
+};
+
+dots.forEach(setDot);
+
+updateDotColors(1, 0);
